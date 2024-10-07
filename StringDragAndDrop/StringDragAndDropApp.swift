6120174a40cab8260cc142121bd332d2f6c5cae7
Eric Kampman <erickampman@yahoo.com>
2024-10-07 07:22:24 -0700

    Initial Commit

---
+++ StringDragAndDrop/StringDragAndDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringDragAndDropApp.swift
+//  StringDragAndDrop
+//
+//  Created by Eric Kampman on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct StringDragAndDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
